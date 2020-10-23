ef39024b64c8cd2b6daa34db92464f421344a96b
DavidSerfaty <david@owanted.io>
2020-10-23 15:27:16 +0200

    Add User and Listing Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Listing.destroy_all
+User.destroy_all
+City.destroy_all
+
+cities = Array.new
+users = Array.new
+listings = Array.new
+
+10.times do
+  city = City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code)
+  cities << city
+end
+puts "10 Cities créées"
+
+20.times do
+  user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.cell_phone_in_e164, description: Faker::Lorem.sentence(word_count: 6), city: cities.sample)
+  users << user
+end
+puts "20 Users créés"
+
+50.times do
+  listing = Listing.create(available_beds: Faker::Number.between(from: 1, to: 5), price: Faker::Number.within(range: 50..200), description: Faker::Lorem.sentence(word_count: 10), has_wifi: Faker::Boolean.boolean, welcome_message: Faker::Lorem.sentence(word_count: 5), city: cities.sample)
+  listings << listing
+end
+puts "50 Listings créés"
